53a4713cd0d15d202977c01187341d48d04da609
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-26 15:27:27 +0200

    [FEAT] creato emit ricerca film

--- src/App.vue
+++ src/App.vue
@@ -15,18 +15,31 @@ import axios from 'axios';
       }
     },
     mounted() {
-      axios.get(store.apiUrl).then((response) => {
-              store.movies = response.data.results;
-              console.log(response.data.results)
-            
-          })
+      this.getMovie()
+    },
+    methods: {
+      getMovie(){
+
+        store.myUrl= store.apiUrl
+        if(store.searchText !== ''){
+             store.myUrl += `&query=${store.searchText}`
+              console.log(store.searchText)
+              
+          }
+
+        axios.get(store.myUrl).then((response) => {
+                store.movies = response.data.results;
+                console.log(response.data.results)
+              
+            })
+      }
     },
     
   }
 </script>
 <template lang="">
   <div>
-    <AppHeader/>
+    <AppHeader @search="getMovie"/>
     <AppMain/>
     
   </div>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,19 @@
 <script>
+import { store } from '../store';
 export default {
+    data() {
+        return {
+            store,
+        }
+    },
     
 }
 </script>
 
 <template lang="">
     <div>
-        <input type="text" id="search" placeholder=" search">
-        <button>Search</button>
+        <input type="text" id="search" v-model="store.searchText" placeholder=" search" >
+        <button @click="$emit('search')">Search</button>
     </div>
 </template>
 
